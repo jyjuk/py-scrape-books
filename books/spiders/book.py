610ab2a8ea9409e6c0fe5ba588b9e1b50e76718c
Ivan Havrylyshyn <jyjuk1@gmail.com>
2025-06-11 19:42:59 +0300

    Fix solution

--- books/spiders/book.py
+++ books/spiders/book.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.http import Response
 import re
+from ..items import BooksItem
 
 
 class BooksSpider(scrapy.Spider):
@@ -11,8 +12,9 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs) -> scrapy.Request:
         for book_pod in response.css(".product_pod"):
             title = book_pod.css("h3 a::attr(title)").get()
-            price = book_pod.css("p.price_color::text").get().strip().replace("£", "")
-
+            price = book_pod.css(
+                "p.price_color::text"
+            ).get().strip().replace("£", "")
             rating_classes = book_pod.css("p.star-rating::attr(class)").get()
             rating = (
                 rating_classes.replace("star-rating ", "").strip()
@@ -27,9 +29,12 @@ class BooksSpider(scrapy.Spider):
                 url=book_detail_url,
                 callback=self.parse_book_details,
                 meta={
-                    "title": title,
-                    "price": price,
-                    "rating": rating,
+                    "item_data": {
+                        "title": title,
+                        "price": price,
+                        "rating": rating,
+                        "url": book_detail_url,
+                    }
                 },
             )
 
@@ -38,40 +43,42 @@ class BooksSpider(scrapy.Spider):
             next_page_url = response.urljoin(next_page_link)
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
-    def parse_book_details(self, response: Response, **kwargs) -> dict:
-        title = response.meta["title"]
-        price = response.meta["price"]
-        rating = response.meta["rating"]
+    def parse_book_details(self, response: Response, **kwargs) -> BooksItem:
+        item = BooksItem()
 
-        upc = response.xpath("//th[text()='UPC']/following-sibling::td/text()").get()
+        initial_data = response.meta["item_data"]
+        item["title"] = initial_data["title"]
+        item["price"] = initial_data["price"]
+        item["rating"] = initial_data["rating"]
+        item["url"] = initial_data["url"]
+
+        upc = response.xpath(
+            "//th[text()='UPC']/following-sibling::td/text()"
+        ).get()
+        item["upc"] = upc
 
         amount_in_stock = None
-
-        stock_info_elements = response.css(".product_main .availability::text").getall()
-
+        stock_info_elements = response.css(
+            ".product_main .availability::text"
+        ).getall()
         full_stock_text = " ".join(
             [text.strip() for text in stock_info_elements if text.strip()]
         )
-
         match = re.search(r"\((\d+)\s+available\)", full_stock_text)
         if match:
             amount_in_stock = int(match.group(1))
+        item["amount_in_stock"] = amount_in_stock
 
         category = response.css(".breadcrumb li:nth-child(3) a::text").get()
+        item["category"] = category
 
         description_selector = response.xpath(
             "//div[@id='product_description']/following-sibling::p/text()"
         )
         description = (
-            description_selector.get().strip() if description_selector.get() else None
+            description_selector.get().strip()
+            if description_selector.get() else None
         )
+        item["description"] = description
 
-        yield {
-            "title": title,
-            "price": price,
-            "amount_in_stock": amount_in_stock,
-            "rating": rating,
-            "category": category,
-            "description": description,
-            "upc": upc,
-        }
+        yield item
